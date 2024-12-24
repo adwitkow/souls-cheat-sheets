d633c7ee73f3ad1ec633c41270320c26ffe49efc
adwitkow <adamadik@poczta.fm>
2024-12-24 15:22:04 +0100

    Add button with source to the footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,19 +1,28 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHeart } from '@fortawesome/free-solid-svg-icons'
 import React from 'react';
-import { Container, Row } from 'react-bootstrap';
+import { Button, Container, Stack } from 'react-bootstrap';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
 
 const Footer = () => {
   return (
     <footer className='bg-body-tertiary p-3 border-top'>
       <Container>
-        <Row>
-          <span>
+        <Stack direction='horizontal'>
+          <div>
             <>Made with </>
             <FontAwesomeIcon icon={faHeart} className='text-danger' />
             <> using React</>
-          </span>
-        </Row>
+          </div>
+          <div className='ms-auto' />
+          <div>
+            <Button
+              href='https://github.com/adwitkow/souls-cheat-sheets/'
+              target='_blank'>
+              <FontAwesomeIcon icon={faGithub} /> Source
+            </Button>
+          </div>
+        </Stack>
       </Container>
     </footer>
   )
