60054979a2557966a12edcd3ce67a917511756ee
adwitkow <adamadik@poczta.fm>
2025-01-07 01:59:31 +0100

    Add progress bars, restyle SectionContainer (into an accordion)

--- src/components/SectionContainer.tsx
+++ src/components/SectionContainer.tsx
@@ -1,12 +1,25 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import { Section } from '../models/section';
 import StepCheckBox from './StepCheckBox';
 import { useProfile } from '../contexts/ProfileContext';
 import { useGame } from '../contexts/GameContext';
+import { Accordion, ProgressBar } from 'react-bootstrap';
 
 interface SectionContainerProps {
   sectionKey: string;
   section: Section;
+}
+
+const countAllSteps = (steps: Steps): number => {
+  return Object.values(steps).reduce((total, step) => {
+    total++;
+
+    if (typeof step !== 'string') {
+      total += countAllSteps(step.steps);
+    }
+
+    return total;
+  }, 0);
 }
 
 const SectionContainer = ({ sectionKey, section }: SectionContainerProps) => {
@@ -20,10 +33,18 @@ const SectionContainer = ({ sectionKey, section }: SectionContainerProps) => {
       return dict;
     }, {} as { [id: string]: boolean });
   });
+  const [stepCount] = useState(countAllSteps(section.steps));
+  const [checkedStepCount, setCheckedStepCount] = useState(() => {
+    return Object.keys(checkedState).filter(key => checkedState[key]).length;
+  })
+  const percentage = useMemo(() => {
+    return checkedStepCount / stepCount * 100;
+  }, [checkedStepCount, stepCount]);
 
   useEffect(() => {
     const checkedKeys = Object.keys(checkedState)
       .filter(key => checkedState[key]);
+    setCheckedStepCount(checkedKeys.length);
 
     saveCheckedSteps(game, sectionKey, checkedKeys);
     // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -35,24 +56,33 @@ const SectionContainer = ({ sectionKey, section }: SectionContainerProps) => {
       childIds.forEach(childId => {
         updatedState[childId] = checked;
       });
-      
+
       return updatedState;
     });
   };
 
   return (
-    <div>
-      <h1>{section.name}</h1>
-      {Object.entries(section.steps).map(([key, content]) => (
-        <StepCheckBox
-          key={key}
-          stepKey={key}
-          stepContent={content}
-          checkedState={checkedState}
-          onChange={handleChange}
-        />
-      ))}
-    </div>
+    <Accordion.Item key={sectionKey} eventKey={sectionKey}>
+      <Accordion.Header>
+        <div style={{ flex: 1 }}>{section.name}</div>
+
+        <div style={{ flex: 2, padding: "0 10px" }}>
+          <ProgressBar now={percentage} label={`${checkedStepCount}/${stepCount}`} />
+        </div>
+      </Accordion.Header>
+      <Accordion.Body>
+        {Object.entries(section.steps).map(([key, content]) => (
+          <StepCheckBox
+            key={key}
+            stepKey={key}
+            stepContent={content}
+            checkedState={checkedState}
+            onChange={handleChange}
+          />
+        ))}
+
+      </Accordion.Body>
+    </Accordion.Item>
   );
 };
 
--- src/pages/playthrough/PlaythroughPage.tsx
+++ src/pages/playthrough/PlaythroughPage.tsx
@@ -1,20 +1,20 @@
 import React from 'react';
 import { useGame } from '../../contexts/GameContext';
 import SectionContainer from '../../components/SectionContainer';
+import { Accordion } from 'react-bootstrap';
 
 const PlaythroughPage = () => {
   const { playthrough } = useGame();
 
   return (
-    <>
+    <Accordion alwaysOpen>
       {Object.entries(playthrough.sections).map(([key, section]) => (
         <SectionContainer
           key={key}
           sectionKey={key}
-          section={section}
-        />
+          section={section} />
       ))}
-    </>
+    </Accordion>
   )
 }
 
